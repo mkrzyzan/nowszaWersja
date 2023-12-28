a90e953f8f81a60618f1fbb301c75542920d8fc8
Marcin Krzyzanowski <marcin.adam.k@gmail.com>
2023-12-28 19:27:21 +0800

    ethers debug

--- assets/js/BlockchainConnection.js
+++ assets/js/BlockchainConnection.js
@@ -29,7 +29,7 @@ async function connectButtonFn() {
             window.signer = signer;
 
             // throw error here
-            throw new Error('No address found');
+            // throw new Error('No address found');
           
             // add the address to the clicked button
             document.getElementById('blockchainConnectBtn').innerHTML = shortAddress;
