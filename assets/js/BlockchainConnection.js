35c2cb30a4f5323e1b54269810975c9923973e81
Marcin Krzyzanowski <marcin.adam.k@gmail.com>
2023-12-28 22:43:20 +0800

    ethers init refactored

--- assets/js/BlockchainConnection.js
+++ assets/js/BlockchainConnection.js
@@ -1,51 +1,32 @@
-document.getElementById('blockchainConnectBtn').addEventListener('click', () => {
-    localStorage.setItem('walletConnected', true)
-    connectButtonFn();
-});
-// call the connectButtonFn function when the DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-    connectButtonFn();
-});
+document.getElementById('blockchainConnectBtn').addEventListener('click', connectButtonFn);
+document.addEventListener('DOMContentLoaded', showAddress);
 
+function showAddress() {
+    const storedAddress = sessionStorage.getItem('address');
+    if (storedAddress) {
+        document.getElementById('blockchainConnectBtn').innerHTML = shortenAddress(storedAddress);
+    } else {
+        document.getElementById('blockchainConnectBtn').innerHTML = 'Connect';
+    }
+}
+
+function shortenAddress(address) {
+    // short the address to 0x1234...5678
+    return address.slice(0, 6) + '...' + address.slice(-4);
+}
 
 async function connectButtonFn() {
     try {
-        console.log('connectButtonFn');
-        if (localStorage.getItem('walletConnected') == 'true') {
-            alert('wallet connected');
+        const provider = new window.ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const address = await signer.getAddress();
 
-            const storedAddress = localStorage.getItem('address');
-            if (storedAddress) {
-                document.getElementById('blockchainConnectBtn').innerHTML = storedAddress;
-            }
+        sessionStorage.setItem('address', address);
 
-            const provider = new window.ethers.BrowserProvider(window.ethereum);
-            const signer = await provider.getSigner();
-            const address = await signer.getAddress();
-
-            // short the address to 0x1234...5678
-            const shortAddress = address.slice(0, 6) + '...' + address.slice(-4);
-
-            localStorage.setItem('address', shortAddress);
-
-            // save the provider and signer to the window object
-            window.provider = provider;
-            window.signer = signer;
-
-            // throw error here
-            // throw new Error('No address found');
-          
-            // add the address to the clicked button
-            document.getElementById('blockchainConnectBtn').innerHTML = shortAddress;
-
-        } else {
-            console.log('wallet not connected');
-            alert('wllet not connected');
-        }
     } catch (error) {
         console.log(error);
-        alert("Error: " + error);
-        localStorage.setItem('walletConnected', false);
-        document.getElementById('blockchainConnectBtn').innerHTML = 'Connect';
+        alert(error);
+        sessionStorage.removeItem('address');
     }
+    showAddress();
 }
